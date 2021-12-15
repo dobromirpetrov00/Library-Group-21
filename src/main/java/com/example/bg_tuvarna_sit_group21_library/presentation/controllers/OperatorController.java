a1a075bb8ef286f67b3203de4cefb277a8bd5f1a
dobromirpetrov00 <godelbit@gmail.com>
2021-12-15 14:53:30 +0200

    update

--- src/main/java/com/example/bg_tuvarna_sit_group21_library/presentation/controllers/OperatorController.java
+++ src/main/java/com/example/bg_tuvarna_sit_group21_library/presentation/controllers/OperatorController.java
@@ -2,14 +2,18 @@ package com.example.bg_tuvarna_sit_group21_library.presentation.controllers;
 
 import com.example.bg_tuvarna_sit_group21_library.constants.Constants;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class OperatorController {
     @FXML
@@ -20,18 +24,47 @@ public class OperatorController {
 
     @FXML
     public void createReaderProfileButtonClick(ActionEvent actionEvent) throws IOException {
-        Stage stage2 = (Stage) goBackButton.getScene().getWindow();
-        stage2.close();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setResizable(false);
+        alert.setTitle("Create Reader Profile");
+        alert.setHeaderText("You are about to create a reader profile!");
+        alert.setContentText("CONTINUE ?");
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constants.LoginView.createReaderProfileView));
-        Parent root = (Parent) fxmlLoader.load();
+        Optional<ButtonType> result = alert.showAndWait();
 
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root));
-        stage.setTitle("Create Reader Profile");
-        stage.setResizable(false);
-        stage.show();
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            try {
+                Stage stage2 = (Stage) goBackButton.getScene().getWindow();
+                stage2.close();
+
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constants.LoginView.createReaderProfileView));
+                Parent root = (Parent) fxmlLoader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Reader Profile");
+                stage.setResizable(false);
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+//    @FXML
+//    public void createReaderProfileButtonClick(ActionEvent actionEvent) throws IOException {
+//        Stage stage2 = (Stage) goBackButton.getScene().getWindow();
+//        stage2.close();
+//
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constants.LoginView.createReaderProfileView));
+//        Parent root = (Parent) fxmlLoader.load();
+//
+//        Stage stage = new Stage();
+//        stage.setScene(new Scene(root));
+//        stage.setTitle("Create Reader Profile");
+//        stage.setResizable(false);
+//        stage.show();
+//    }
 
     @FXML
     public void goBackButtonClick(ActionEvent actionEvent) throws IOException {
