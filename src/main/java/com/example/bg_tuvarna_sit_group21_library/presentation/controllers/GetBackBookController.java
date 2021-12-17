74388d183782c932439407609896f6f806eb9d28
dobromirpetrov00 <godelbit@gmail.com>
2021-12-17 19:55:15 +0200

    update

--- src/main/java/com/example/bg_tuvarna_sit_group21_library/presentation/controllers/GetBackBookController.java
+++ src/main/java/com/example/bg_tuvarna_sit_group21_library/presentation/controllers/GetBackBookController.java
@@ -1,4 +1,57 @@
 package com.example.bg_tuvarna_sit_group21_library.presentation.controllers;
 
+import com.example.bg_tuvarna_sit_group21_library.constants.Constants;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class GetBackBookController {
+    @FXML
+    public Button getBackBookButton;
+
+    @FXML
+    public Button goBackButton;
+
+    @FXML
+    public TextField bookId;
+
+    @FXML
+    public TextField rdUserId;
+
+    @FXML
+    public TextField rdUsername;
+
+    @FXML
+    public TextField rdPassword;
+
+    @FXML
+    public Label wrongLabel;
+
+    @FXML
+    public void getBackBookButtonClick(ActionEvent actionEvent) {
+        
+    }
+
+    @FXML
+    public void goBackButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage2 = (Stage) goBackButton.getScene().getWindow();
+        stage2.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constants.LoginView.operatorView));
+        Parent root = (Parent) fxmlLoader.load();
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Operator Menu");
+        stage.setResizable(false);
+        stage.show();
+    }
 }
